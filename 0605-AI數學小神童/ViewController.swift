9d458f58b42168e485b22870b239676650103515
moon <wings0411@gmail.com>
2018-06-07 20:19:10 +0800

    1.0

--- 0605-AI數學小神童/ViewController.swift
+++ 0605-AI數學小神童/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
+
+var num1:Double? = 0
+var num2:Double? = 0
+var sum:Double? = 0
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var mySegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var num2TextField: UITextField!
+    @IBOutlet weak var num1TextField: UITextField!
+    @IBAction func choiceSegmentedcontrol(_ sender: UISegmentedControl) {
+        
+        num1 = Double(num1TextField.text!)!
+        num2 = Double(num2TextField.text!)!
+        
+        if num1 != nil && num2 != nil {
+            calculate()
+            answerLabel.text = sum!.description
+            answerLabel.text = String(format: "%.2f", sum!)
+            let speechUtterance =  AVSpeechUtterance(string: answerLabel.text!)
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+            let synth = AVSpeechSynthesizer()
+            synth.speak(speechUtterance)
+        }
+    }
+    
+    
+    func calculate() {
+        if mySegmentedControl.selectedSegmentIndex == 0 {
+            sum! = num1! + num2!
+            //print(sum!)
+        } else if mySegmentedControl.selectedSegmentIndex == 1 {
+            sum! = num1! - num2!
+            //print(sum!)
+        } else if mySegmentedControl.selectedSegmentIndex == 2 {
+            sum! = num1! * num2!
+            //print(sum!)
+        } else if mySegmentedControl.selectedSegmentIndex == 3 {
+            sum! = num1! / num2!
+            //print(sum!)
+        }
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
     }
 
     override func didReceiveMemoryWarning() {
